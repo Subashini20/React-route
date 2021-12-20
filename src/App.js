a7eb7ab8f41c279cfd899dc14de618acae294b2c
Subashini20 <78964674+Subashini20@users.noreply.github.com>
2021-12-20 19:17:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from 'react';
 import {
   BrowserRouter,
   Route,
-  Link,
   Switch,
-  NavLink,
-  useHistory,
-  useLocation,
-  useParams,
-  useRouteMatch,
 } from 'react-router-dom';
 import Designcomponent from './Design';
 import {AboutUs}  from './Design';
